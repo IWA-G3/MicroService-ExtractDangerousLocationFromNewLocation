56ceca9932a198486d157a3bc0728b922b9fb73b
Adrien Alexandre <alexandreadrien@orange.fr>
2020-11-06 13:59:32 +0100

    Init project

---
+++ src/main/java/polytech/group3/iwa/ExtractfromlocaitonApplication.java
@@ -0,0 +1,13 @@
+package polytech.group3.iwa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExtractfromlocaitonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExtractfromlocaitonApplication.class, args);
+	}
+
+}
