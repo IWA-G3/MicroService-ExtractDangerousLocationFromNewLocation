7d7c4578cd6dd271ef2664099468be16b5669373
Adrien Alexandre <alexandreadrien@orange.fr>
2020-11-08 15:42:07 +0100

    Basic Producer which publish string

--- src/main/java/polytech/group3/iwa/ExtractFromLocationApplication.java
+++ src/main/java/polytech/group3/iwa/ExtractFromLocationApplication.java
@@ -1,8 +1,10 @@
 package polytech.group3.iwa;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.core.KafkaTemplate;
 
 @SpringBootApplication
 public class ExtractFromLocationApplication {
@@ -14,5 +16,12 @@ public class ExtractFromLocationApplication {
 	@KafkaListener(topics = "location", groupId = "DangerLocationFromLocationListener")
 	public void listen(String message) {
 		System.out.println("Received Messages in group - DangerLocationFromLocationListener: " + message);
+	}
+
+	@Autowired
+	private KafkaTemplate<String, String> kafkaTemplate;
+
+	public void sendMessage(String msg) {
+		kafkaTemplate.send("dangerous_location", msg);
 	}
 }
